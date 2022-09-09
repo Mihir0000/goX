381065071372f6546c2d369f5f1b47a866e467be
Aditi087 <aditijana0@gmail.com>
2022-09-09 15:18:49 +0530

    change confirm password

--- frontend/src/components/authentication/Registration.jsx
+++ frontend/src/components/authentication/Registration.jsx
@@ -3,35 +3,60 @@ import { Container, Modal, Button } from "react-bootstrap";
 
 import swal from "sweetalert";
 import "./form.css";
-
+import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-// import Header from "../header/Header";
-import { Paper, input } from "@mui/material";
 
 export default function Registration() {
-  
   const validateEmail = RegExp(
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   );
+  const validPassword = RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,20}$/);
 
   const [inputState, setInputState] = useState({
-    email: "",
-    password: "",
+    isError: {
+      userName: "",
+      userEmail: "",
+      password: "",
+      // confirmPassword:""
+    },
   });
   const [error, setError] = useState({});
+  const [confirm, setConfirm] = useState()
   let name, value;
   const handleChange = (event) => {
+    event.persist();
     name = event.target.name;
     value = event.target.value;
     setInputState({ ...inputState, [name]: value });
+    setConfirm(event.target.value)
   };
+
   const validation = () => {
     let error = {};
-    if (!inputState.email) {
-      error.email = "Email is required";
+    if (!inputState.userName) {
+      error.userName = "Username is required";
+    } else if (inputState.userName.length < 3) {
+      error.userName = "At least 3 characters";
+    } else if (/\d/.test(inputState.userName)) {
+      error.userName = "Please enter a valid Username";
+    }
+    if (!inputState.userEmail) {
+      error.userEmail = "Email is required";
+    } else if (!validateEmail.test(inputState.userEmail)) {
+      error.userEmail = "Invalid Email";
     }
     if (!inputState.password) {
       error.password = "Enter password";
+    } 
+    // else if (!validPassword.test(inputState.password)) {
+    //   error.password =
+    //     "atleast 1 uppercase 1 lowercase and 1 number minimum 8 characters";
+    // }
+    if (!confirm) {
+      error.confirm = "Confirm password";
+    } else if (inputState.password !== confirm) {
+      error.confirm =
+        "password doesn't match";
     }
 
     return error;
@@ -45,17 +70,47 @@ export default function Registration() {
     setError(validation());
     if (Object.keys(ErrorList).length !== 0) {
     } else {
-      localStorage.setItem("email", inputState.email);
-      localStorage.setItem("password", inputState.password);
-      localStorage.setItem("status", true);
-      swal(
-        "Good job!",
-        "A Verification Code has been sent to your email",
-        "success"
-      );
-      navigate("/login");
-    }
+    axios
+      .post("http://localhost:5000/register", value)
+      .then((res) => {
+        console.log("Axios res: ", res);
+        swal(
+          "Good job!",
+          "A Verification Code has been sent to your userEmail",
+          "success"
+        );
+        setInputState({
+          userName: "",
+          userEmail: "",
+          password: "",
+        });
+        setConfirm("")
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("Already Registered");
+      });
   };
+}
+
+  // const submitHandler1 = (event) => {
+  //   event.preventDefault();
+  //   let ErrorList = validation();
+  //   setError(validation());
+  //   if (Object.keys(ErrorList).length !== 0) {
+  //   } else {
+  //     localStorage.setItem("userEmail", inputState.userEmail);
+  //     localStorage.setItem("password", inputState.password);
+  //     localStorage.setItem("status", true);
+  //     swal(
+  //       "Good job!",
+  //       "A Verification Code has been sent to your userEmail",
+  //       "success"
+  //     );
+  //     navigate("/login");
+  //   }
+  // };
 
   return (
     <>
@@ -67,19 +122,38 @@ export default function Registration() {
 
               <input
                 className="input"
-                type="email"
-                name="email"
-                value={inputState.email}
+                type="text"
+                name="userName"
+                value={inputState.userName}
                 onChange={handleChange}
                 fullWclassNameth
-                placeholder="Email"
+                placeholder="Enter a username"
                 sx={{
                   wclassNameth: "80%",
                   margin: "40px 0",
                 }}
                 variant="standard"
               />
-              <p className="Lerror">{error.email}</p>
+              <br />
+              <span className="err">{error.userName}</span>
+              <br />
+              <input
+                className="input"
+                type="email"
+                name="userEmail"
+                value={inputState.userEmail}
+                onChange={handleChange}
+                fullWclassNameth
+                placeholder="Enter your email"
+                sx={{
+                  wclassNameth: "80%",
+                  margin: "40px 0",
+                }}
+                variant="standard"
+              />
+              <br />
+              <span className="err">{error.userEmail}</span>
+              <br />
 
               <input
                 className="input"
@@ -89,11 +163,26 @@ export default function Registration() {
                 onChange={handleChange}
                 fullWclassNameth
                 sx={{ wclassNameth: "80%" }}
-                placeholder="Password"
+                placeholder="Create Password"
                 variant="standard"
               />
-              <p className="Lerror">{error.password}</p>
-
+              <br />
+              <span className="err">{error.password}</span>
+              <br />
+              <input
+                className="input"
+                type="password"
+                name="cPassword"
+                value={inputState.confirmPassword}
+                onChange={handleChange}
+                fullWclassNameth
+                sx={{ wclassNameth: "80%" }}
+                placeholder="Confirm Password"
+                variant="standard"
+              />
+              <br />
+              <span className="err">{error.confirm}</span>
+              <br />
               <br />
               <Button
                 className="logBtn"
